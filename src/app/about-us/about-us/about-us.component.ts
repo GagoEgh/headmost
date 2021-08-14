83c25fa7bb4b388c2e35439c46119115913ebf3f
GagoEgh <egagik69@gmail.com>
2021-08-14 17:50:07 +0400

    close-open

--- src/app/about-us/about-us/about-us.component.ts
+++ src/app/about-us/about-us/about-us.component.ts
@@ -1,17 +1,23 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { FramesServService } from 'src/app/shared/frames-serv.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent implements OnInit, AfterViewChecked {
+  public _unsubscribe$ = new Subject();
   @ViewChild("block", { static: false }) block: ElementRef | undefined;
   heigth: number | undefined;
   width: number | undefined;
   scale: number = 1;
-
+  isBlock: boolean = false;
+  question_answer: any[] = [];
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.heigth = this.block?.nativeElement.clientHeight | 1;
@@ -43,9 +49,33 @@ export class AboutUsComponent implements OnInit {
     return style
   }
 
-  constructor(public frames:FramesServService) { }
 
-  ngOnInit(): void {
+
+  constructor(public frames: FramesServService, public _translate: TranslateService) { }
+  ngAfterViewChecked(): void {
+    this._translate.use(this.frames.lang);
+    this.question_answer = [];
+    this._translate.get('_about._questions').pipe(takeUntil(this._unsubscribe$)).subscribe((res: any) => {
+      this.question_answer.push(...res)
+    });
+    
   }
 
+  ngOnInit(): void {
+    this._translate.get('_about._questions').pipe(takeUntil(this._unsubscribe$)).subscribe((res: any) => {
+      this.question_answer.push(...res)
+    })
+  }
+
+  showQuestion(ev:any,num:number){
+    this.isBlock = !this.isBlock;
+    ev.id = !!!ev.id;
+    console.log(ev)
+
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 }
