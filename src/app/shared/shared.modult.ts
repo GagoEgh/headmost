b725d959ebbae7a44285647fa5bef58b3b75bc7b
Gagik Eghiazaryan <egagik69@gmail.com>
2021-11-27 15:05:55 +0400

    shared

--- src/app/shared/shared.modult.ts
+++ src/app/shared/shared.modult.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzFormModule } from "ng-zorro-antd/form";
@@ -8,63 +8,67 @@ import { NzSelectModule } from "ng-zorro-antd/select";
 import { NgxMaskModule } from "ngx-mask";
 import { OrderComponent } from "../order/order/order.component";
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import { MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 
 import { NgxSpinnerModule } from "ngx-spinner";
-import {TranslateModule} from '@ngx-translate/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+const MatModule = [
+    MatButtonModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+]
+
+const NzModule = [
+    NzModalModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzSelectModule,
+    NzDatePickerModule,
+]
+
+const NgxModule = [
+    NgxTrimDirectiveModule,
+    NgxSpinnerModule,
+]
+
+const module = [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+]
 
 @NgModule({
-    declarations:[OrderComponent],
-    exports:[OrderComponent,NzModalModule,
-        MatButtonModule,
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        NzFormModule ,
-        NzButtonModule,
-        NzInputModule,
-        NzSelectModule,
-        NzDatePickerModule,
-        MatDatepickerModule,
-        MatInputModule,
-        MatNativeDateModule,
-        NgxTrimDirectiveModule,
-        MatFormFieldModule,
+    declarations: [OrderComponent],
+    exports: [
+        ...MatModule,
+        ...NzModule,
+        ...NgxModule,
+        ...module,
         NgxMaskModule,
-        NgxSpinnerModule,
-        TranslateModule,
-        MatSelectModule
+        OrderComponent,
     ],
-    imports:[
-        MatButtonModule,
-        TranslateModule,
-        MatSelectModule,
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        NzFormModule ,
-        NzButtonModule,
-        NzInputModule,
-        NzSelectModule,
-        NzDatePickerModule,
-        MatDatepickerModule,
-        MatInputModule,
-        MatNativeDateModule,
-        NgxTrimDirectiveModule,
-        NzModalModule,
-        MatFormFieldModule,
-        NgxSpinnerModule,
+    imports: [
+        ...module,
+        ...NzModule,
+        ...MatModule,
+        ...NgxModule,
         NgxMaskModule.forRoot(),
     ],
-  
+
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    
+
 })
-export class SharedModule{}
+export class SharedModule { }
